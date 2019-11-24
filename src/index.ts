fb77d7092d8b33dd235100698a4cfbbf06c22920
Yamaji <ky2000create@gmail.com>
2019-11-24 09:54:51 +0900

    Lintエラー修正

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,44 @@
 import express from "express";
 import * as bodyParser from "body-parser";
 //import * as path from 'path';
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
+import browserSync from "browser-sync";
 
-let defaultPort = 3000;
+const defaultPort = 3000;
 const app = express();
-const router = express.Router();
-app.set('views', './views')
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // app.use("/", router);
-if (process.env.NODE_ENV !== 'production') {
-  let browserSync = require('browser-sync').create();
+if (process.env.NODE_ENV !== "production") {
   browserSync.init({
     watch: true,
     open: false,
     logSnippet: false,
     proxy: `localhost:${defaultPort}`,
-    files: ['./views/**/*', './public/**/*']
+    files: ["./views/**/*", "./public/**/*"]
   });
 }
-app.set('port', defaultPort);
-app.get('/', (req, res, next) => {
-  res.render('pages/index', {
-    title: 'Sample',
-    message: 'Welcome to Website'
+app.set("port", defaultPort);
+app.get(
+  "/",
+  (req, res, next) => {
+    res.render("pages/index", {
+      title: "Sample",
+      message: "Welcome to Website"
+    });
+    console.log(req.query);
+    next();
+  },
+  () => {
+    console.log("callback2");
   }
- );
- console.log(req.query);
- next();
-}, (req, res) => {
-  console.log("callback2");
-});
+);
 
 // app.use("/", router);
 
